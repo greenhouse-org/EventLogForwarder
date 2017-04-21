848f9633fdf1eb10c633c5508426f8bcde902145
Sunjay Bhatia <sbhatia@pivotal.io>
2017-04-21 18:14:03 -0400

    initial commit
    [#143824097]

---
+++ Tailer/Tailer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tailer
+{
+    public class Tailer
+    {
+        public class NonexistantEventSourceException : Exception {}
+        public class NonexistantEventLogException : Exception { }
+
+        public void Start()
+        {
+
+        }
+    }
+}
