424b8d9982a17e4eee254cb8377874faeb68c49d
Natalie Arellano <narellano@pivotal.io>
2017-04-28 10:39:32 -0400

    added log forwarding udp functionality as command line program
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- EventLogForwarder/Program.cs
+++ EventLogForwarder/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Forwarder;
+using System;
+using System.Diagnostics;
+using System.Threading;
 using Tailer;
 
 namespace EventLogForwarder
@@ -11,8 +10,38 @@ namespace EventLogForwarder
     {
         public static void Main(string[] args)
         {
-            var tailer = new Tailer();
+            if (args.Length != 2)
+                throw new ArgumentException("Usage: url port");
+            var url = args[0];
+            var port = int.Parse(args[1]);
 
+            string[] logsToListenTo = new[] { "Application", "Security", "System" };
+            var forwarder = new UDPForwarder(url, port);
+            var stdout = Console.Out;
+            var stderr = Console.Error;
+
+            foreach(var logToListenTo in logsToListenTo)
+            {
+                var subscription = new EventLogSubscription(logToListenTo, forwarder.Write, stdout, stderr);
+                subscription.Start();
+            }
+
+            WriteAMessageToTheEventLog($"Connected Event Log Forwarding");
+
+            while (true)
+            {
+                Thread.Yield();
+            }
+        }
+
+        private static void WriteAMessageToTheEventLog(string message)
+        {
+            short category = 0;
+            EventLogEntryType type = EventLogEntryType.Error;
+            int eventID = 1000;
+            var sourceName = AppDomain.CurrentDomain.FriendlyName;
+
+            EventLog.WriteEntry(sourceName, message, type, eventID, category);
         }
     }
 }
